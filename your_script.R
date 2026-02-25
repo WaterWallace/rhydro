02d1d0f79c65bc574589920a9684e842ca59b63b
Stephen Wallace <107374774+WaterWallace@users.noreply.github.com>
2026-02-25 11:34:32 +1000

    Create your_script.R

---
+++ your_script.R
@@ -0,0 +1,60 @@
+
+
+getCR6json2 <- function(username, pass, logger, port, suffix, timeout = NULL, maxretry = 10, retrynum = 0)
+{
+    
+    url <- paste("http://",username,":",pass,"@",logger,":",port,suffix,sep="")
+    print(url)
+    
+    if(is.null(timeout)){
+        httpTimeout <- httr::timeout((retrynum+1)*2)
+    }else{
+        httpTimeout <- httr::timeout(timeout)
+    }
+    
+    jsondata <- tryCatch(
+        {
+            httr::GET(url = url, httpTimeout)
+        },
+        error = function(e){
+            message("Error:")
+            print(e)
+            return(NULL)
+        },
+        finally = {
+            
+        }
+    )
+    #jsondata$content
+    
+    
+    if ( is.null(jsondata) ){
+        if(retrynum >= maxretry) return( NULL )
+        Sys.sleep(retrynum*2)
+        message(paste("retry",retrynum,"of",maxretry))
+        getCR6json2(username, pass, logger, port, suffix, maxretry = maxretry, retrynum = retrynum + 1)
+    } 
+    
+    message("Succesfully connected")
+    if(jsondata$status_code != 200) {
+        message(paste("Bad status code:", jsondata$status_code ) )
+        if(retrynum >= maxretry) return(jsondata$status_code)
+        message(paste("retry",retrynum,"of",maxretry))
+        getCR6json2(username, pass, logger, port, suffix, maxretry = maxretry, retrynum = maxretry)
+    }
+    
+    message("Succesfully retrieved file list")
+    return(jsonlite::fromJSON(jsondata$content %>% rawToChar))
+}
+
+suffix <- "/CRD/?command=ListFiles&format=json"
+
+logger <- "1120053wqi.is-a-techie.com"
+#logger <- "wetlandtsl5.ddns.net"
+username <- "readonlyuser"
+pass <- "wetlands"
+port <- 997
+suffix <- "/CRD/?command=ListFiles&format=json"
+
+fileslist <- getCR6json2(username, pass, logger, port, suffix, timeout = NULL)
+saveRDS(fileslist, "fileslist.RDS")
